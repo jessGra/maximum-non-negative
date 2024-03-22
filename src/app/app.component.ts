d8697a96bcd4b56e39bb7c227cc40a4f40a213de
JesGra <jesus.egranada@gmail.com>
2024-03-22 10:02:29 -0500

    add bootstrap dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NgbAlertModule } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NgbAlertModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  title = 'maximum-non-negative';
+  title = "maximum-non-negative";
 }
