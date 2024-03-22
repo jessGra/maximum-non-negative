5ac00e3e35c792fa918e666f2b220eba91e4341f
JesGra <jesus.egranada@gmail.com>
2024-03-22 11:25:56 -0500

    create layout components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { NgbAlertModule } from "@ng-bootstrap/ng-bootstrap";
+import { MainContentComponent } from "./core/main-content/main-content.component";
+import { FooterComponent } from "./core/footer/footer.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, NgbAlertModule],
+  imports: [MainContentComponent, FooterComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
